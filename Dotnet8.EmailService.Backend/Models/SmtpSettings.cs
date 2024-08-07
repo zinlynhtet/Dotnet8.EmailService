dfe6d48fe0ba35af1b5c6a1e9eccca8db80a9559
ZinLinnhtet <mackk5504@gmail.com>
2024-08-07 11:32:09 +0630

    perf: move specified file type

---
+++ Dotnet8.EmailService.Backend/Models/SmtpSettings.cs
@@ -0,0 +1,8 @@
+﻿public class SmtpSettings
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string User { get; set; }
+    public string Pass { get; set; }
+    public bool EnableSsl { get; set; }
+}
